63c318e930c23af593ad834109dad1e15ae4c1b0
ccandradewi <cornelia.candradewi@outlook.com>
2024-04-12 19:07:22 +0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <main className=' flex flex-col items-center max-w-sm p-11 rounded overflow-hidden border border-gray-300' >
+      <img src='https://logos-world.net/wp-content/uploads/2020/05/Instagram-Logo-2016-present.png' className=' w-28 item-center' ></img>
+      <form className=' flex flex-col gap-3'>
+      <input placeholder='username' className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-grey-500 bg-gray-100 text-black' ></input>
+      <input placeholder='password' className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-grey-500 bg-gray-100 text-black'></input>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded '>Masuk</button>
+      <Link href='' className='text-blue-500'> Forget password? </Link>
+      </form>
+      <p className=' mt-4 '>Don't have an account?</p><Link href='/register' className=' text-blue-500 '>Sign up</Link>
+    </main>
+  );
+}
