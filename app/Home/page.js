e1428cd190c41772663a2e63c501a2d4e45b0239
ccandradewi <cornelia.candradewi@outlook.com>
2024-04-12 23:40:32 +0700

    add navbar

---
+++ app/Home/page.js
@@ -0,0 +1,10 @@
+import Sidebar from "../component/sidebar";
+
+export default function Home() {
+    return(
+        <div>
+            <Sidebar/>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
       <form className=' flex flex-col gap-3'>
       <input placeholder='username' className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-grey-500 bg-gray-100 text-black' ></input>
       <input placeholder='password' className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-grey-500 bg-gray-100 text-black'></input>
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded '>Masuk</button>
+      <Link href='/Home'>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded '>Login</button>
+      </Link>
       <Link href='' className='text-blue-500'> Forget password? </Link>
       </form>
       <p className=' mt-4 '>Don't have an account?</p><Link href='/register' className=' text-blue-500 '>Sign up</Link>
